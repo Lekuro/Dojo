6418550b18a7fc7b0644255f813e72b52310da5d
Lekuro <lekurot@gmail.com>
2018-03-23 11:24:43 +0200

    separate html css js

---
+++ pacman/main.js
@@ -0,0 +1,67 @@
+var world = [
+  [1, 1, 1, 1, 1],
+  [1, 0, 2, 2, 1],
+  [1, 2, 1, 2, 1],
+  [1, 2, 2, 2, 1],
+  [1, 2, 2, 2, 1],
+  [1, 2, 1, 2, 1],
+  [1, 2, 2, 2, 1],
+  [1, 2, 2, 2, 1],
+  [1, 2, 1, 2, 1],
+  [1, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1]
+];
+var worldDict = {
+  0: "block blank",
+  1: "block wall",
+  2: "block sushi"
+};
+function drawWorld() {
+  output = "";
+  for (var row = 0; row < world.length; row++) {
+    output += '<div class="row">';
+    for (var x = 0; x < world[row].length; x++) {
+      output += '<div class="' + worldDict[world[row][x]] + '"></div>';
+      //console.log("value: ", worldDict[world[row][x]]);
+    }
+    output += "</div>";
+  }
+  document.getElementById("world").innerHTML = output;
+}
+drawWorld();
+
+var ninjaman = {
+  x: 1,
+  y: 1
+};
+function drawNinjaman() {
+  document.getElementById("ninjaman").style.top = ninjaman.y * 40 + "px";
+  document.getElementById("ninjaman").style.left = ninjaman.x * 40 + "px";
+}
+drawNinjaman();
+document.onkeydown = function(e) {
+  if (e.keyCode == 37) {
+    if (world[ninjaman.y][ninjaman.x - 1] != 1) {
+      ninjaman.x--;
+    }
+  }
+  if (e.keyCode == 39) {
+    if (world[ninjaman.y][ninjaman.x + 1] != 1) {
+      ninjaman.x++;
+    }
+  }
+  if (e.keyCode == 38) {
+    if (world[ninjaman.y - 1][ninjaman.x] != 1) {
+      ninjaman.y--;
+    }
+  }
+  if (e.keyCode == 40) {
+    if (world[ninjaman.y + 1][ninjaman.x] != 1) {
+      ninjaman.y++;
+    }
+  }
+  //console.log(ninjaman, world[ninjaman.y][ninjaman.x]);
+  world[ninjaman.y][ninjaman.x] = 0;
+  drawWorld();
+  drawNinjaman();
+};
